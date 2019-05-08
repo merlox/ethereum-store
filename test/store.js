3aad6134260273de5906d409694571f306cbd741
merlox <merunasgrincalaitis@gmail.com>
2019-05-08 18:55:29 +0200

    Passing first test

--- test/store.js
+++ test/store.js
@@ -25,19 +25,19 @@ contract('Store', accounts => {
         await identityRegistry.createIdentity(accounts[1], [accounts[2]], [accounts[2]], { from: accounts[1], gas: 8e6 })
     })
     it('should create a product successfully', async () => {
-        // const title = 'This is an example'
-        // const sku = bytes32('2384jd93nf')
-        // const description = 'This is the description of the product'
-        // const price = 200
-        // const image = 'https://example.com'
-        // const attributes = [bytes32('size'), bytes32('color')]
-        // const attributeValues = [bytes32('s'), bytes32('m'), bytes32('x'), bytes32('red'), bytes32('blue'), bytes32('green')]
-        // const quantity = 5
-        //
-        // // Create the product
-        // await store.publishProduct(title, sku, description, price, image, attributes, attributeValues, quantity)
-        // const product = await store.products(0)
-        // console.log('Product', product)
+        const title = 'This is an example'
+        const sku = bytes32('2384jd93nf')
+        const description = 'This is the description of the product'
+        const price = 200
+        const image = 'https://example.com'
+        const attributes = [bytes32('size'), bytes32('color')]
+        const attributeValues = [bytes32('s'), bytes32('m'), bytes32('x'), bytes32('red'), bytes32('blue'), bytes32('green')]
+        const quantity = 5
+
+        // Create the product
+        await store.publishProduct(title, sku, description, price, image, attributes, attributeValues, quantity)
+        const product = await store.products(0)
+        assert.equal(title, product.title, 'The product must be deployed with the publish function')
     })
     it('should purchase a product successfully')
     it('should delete a product successfully')
