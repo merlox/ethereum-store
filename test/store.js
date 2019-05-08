5f2ce56e804eab385c4d8d87bf3dfdb87c0ffd35
merlox <merunasgrincalaitis@gmail.com>
2019-05-08 18:04:24 +0200

    Tests compiling successfully

--- test/store.js
+++ test/store.js
@@ -2,8 +2,10 @@ const assert = require('assert')
 const Store = artifacts.require('Store.sol')
 const IdentityRegistry = artifacts.require('IdentityRegistry.sol')
 const Token = artifacts.require('HydroTokenTestnet.sol')
+const Dispute = artifacts.require('Dispute.sol')
 let store
 let token
+let dispute
 let identityRegistry
 
 contract('Store', accounts => {
@@ -12,9 +14,10 @@ contract('Store', accounts => {
         token = await Token.new()
         identityRegistry = await IdentityRegistry.new()
         store = await Store.new(token.address, identityRegistry.address)
+        dispute = await Dispute.new(store.address)
         console.log(token.address, identityRegistry.address)
     })
     it('a', () => {
-        
+
     })
 })
